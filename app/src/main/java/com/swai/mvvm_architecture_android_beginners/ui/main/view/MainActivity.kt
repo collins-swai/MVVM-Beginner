668a80ad75077ed11cc6ac2c8b49b2a0acfbd24d
Collins Swai <kipkorirswai@gmail.com>
2022-07-06 12:26:33 +0300

    Entry point for app

---
+++ app/src/main/java/com/swai/mvvm_architecture_android_beginners/ui/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.swai.mvvm_architecture_android_beginners.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swai.mvvm_architecture_android_beginners.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
